8cd51fd3a7984af7f8f2a22b38989bf116c70cca
jeremy <jeremyaleman468@gmail.com>
2023-12-15 18:01:33 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function customerName(){
+ return customerName; 
+}
+
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'  
+}
+
+const leastFavoriteCustomer = "Joseph"
+function changeLeastFavoriteCustomer(){
+ leastFavoriteCustomer = "John";
+}
